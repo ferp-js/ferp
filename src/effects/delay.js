6f8be545834756fd31d8d8e751b983679e9b804c
Alex Barry <alex.barry@gmail.com>
2018-09-03 08:59:01 -0400

    Added requestAnimationFrame delay effect for games.

--- src/effects/delay.js
+++ src/effects/delay.js
@@ -8,9 +8,16 @@ const second = (seconds, messageType) => millisecond(seconds * 1000, messageType
 const minute = (minutes, messageType) => second(minutes * 60, messageType);
 const hour = (hours, messageType) => minute(hours * 60, messageType);
 
+const raf = (messageType) => new Effect((done) => {
+  window.requestAnimationFrame((timestamp) => {
+    done({ type: messageType, timestamp });
+  });
+});
+
 module.exports = {
   millisecond,
   second,
   minute,
   hour,
+  raf,
 };
