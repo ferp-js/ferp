57b1d5cebb118eb1f1c72f44817b5e22dda2f77a
Alex Barry <alex.barry@gmail.com>
2020-03-07 10:54:15 -0500

    Upgrade rollup, ava, and eslint dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,20 @@
 import { eslint } from 'rollup-plugin-eslint';
 import minify from 'rollup-plugin-babel-minify';
+import strip from '@rollup/plugin-strip';
+import cleanup from 'rollup-plugin-cleanup';
 
 export default {
   input: './src/ferp.js',
   plugins: [
     eslint(),
     minify(),
+    strip(),
+    cleanup(),
   ],
   output: {
     name: 'ferp',
     file: 'ferp.js',
     format: 'umd',
+    exports: 'named',
   },
 };
