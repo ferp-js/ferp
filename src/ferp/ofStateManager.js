81737fc949f3bf8edbc1c8e3b5dc79b19b21a881
Alex Barry <alex.barry@gmail.com>
2020-08-30 12:13:42 -0400

    New ofStateManager

---
+++ src/ferp/ofStateManager.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-restricted-syntax, no-continue */
+
+function* ofStateGenerator(dispatch, ofState) {
+  const generators = [...ofState];
+  const completed = [];
+
+  while (true) {
+    const state = yield;
+    if (typeof state === 'undefined') continue;
+
+    for (const generator of generators) {
+      if (completed.indexOf(generator) >= 0) continue;
+
+      const iter = generator.next(state);
+      if (iter.done) {
+        completed.push(generator);
+      }
+      if (Array.isArray(iter.value)) {
+        for (const message of iter.value) {
+          dispatch(message);
+        }
+      }
+    }
+  }
+}
+
+export const ofStateManager = (dispatch, initialState, ofState) => {
+  const generator = ofStateGenerator(dispatch, ofState);
+  generator.next();
+  generator.next(initialState);
+
+  return {
+    next: (state) => generator.next(state),
+    return: (state) => {
+      for (const g of ofState) {
+        g.return(state);
+      }
+      return generator.return(state);
+    },
+  };
+};
