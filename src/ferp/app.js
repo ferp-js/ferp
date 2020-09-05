b74380790b6f681232b6091b1c0eab6d2c8c2e77
Alex Barry <alex.barry@gmail.com>
2020-09-05 16:01:42 -0400

    Got the generators to place mostly nice together

--- src/ferp/app.js
+++ src/ferp/app.js
@@ -6,19 +6,45 @@ import { ofStateManager } from './ofStateManager.js';
 export const app = ({
   init,
   ofState,
+  observe,
 }) => {
   const state = stateManager();
   const messages = messageManager();
-  const manageEffects = effectManager(messages.dispatch);
-  const manageOfState = ofStateManager(messages.dispatch, ofState);
 
-  const runUpdate = ([nextState, nextEffect]) => {
+  const observeFn = observe || (() => {});
+  const dispatch = (params) => {
+    setTimeout(() => {
+      messages.dispatch(params);
+    }, 0);
+  };
+
+  const manageEffects = effectManager(dispatch);
+  const manageOfState = ofStateManager(dispatch, init[0], ofState);
+
+  const runUpdate = (message) => {
+    observeFn(message);
+    if (typeof message === 'function') {
+      dispatch(message(state.get()));
+      return;
+    }
+    const [nextState, nextEffect] = message;
+    if (!nextEffect) {
+      throw new ReferenceError(`App.init or each action must be an [state, effect] tuple, but you gave [${nextState}, ${nextEffect}]`);
+    }
     state.set(nextState);
     manageOfState.next(nextState);
-    return manageEffects(nextEffect);
+    manageEffects.next(nextEffect);
   };
+
+  messages.onDispatch(runUpdate);
 
   runUpdate(init);
 
-  return messages.dispatch;
+  return {
+    dispatch,
+    detach: () => {
+      manageEffects.return();
+      manageOfState.return();
+    },
+  };
 };
--- src/ferp/effectManager.js
+++ src/ferp/effectManager.js
@@ -1,20 +1,38 @@
 import { effectTypes } from './effects/core.js';
 
-const effectHandler = {
-  [effectTypes.none]: () => Promise.resolve(),
-  [effectTypes.batch]: (effect, manager) => effect.effects.reduce((chain, fx) => (
-    chain.then(() => manager(fx))
-  ), Promise.resolve()),
-  [effectTypes.defer]: (effect, manager) => effect.promise.then(manager),
-  [effectTypes.thunk]: (effect, manager) => manager(effect.method()),
+export const runEffect = (dispatch, effect) => {
+  switch (effect && effect.type) {
+    case effectTypes.none:
+      return undefined;
+
+    case effectTypes.batch:
+      return effect.effects.forEach((fx) => runEffect(dispatch, fx));
+
+    case effectTypes.defer:
+      return Promise.resolve(effect.promise).then((fx) => runEffect(dispatch, fx));
+
+    case effectTypes.thunk:
+      return runEffect(dispatch, effect.method());
+
+    default:
+      try {
+        return dispatch(effect);
+      } catch (err) {
+        console.log('unable to run effect', effect, err);
+      }
+  }
 };
 
-export const effectManager = (dispatch) => {
-  const manager = (effect) => {
-    const handler = effectHandler[effect && effect.type];
-    if (handler) return handler(effect, manager);
-    return Promise.resolve(dispatch(effect));
-  };
+function* effectGenerator(dispatch) {
+  while (true) {
+    const pendingEffect = yield;
+    if (!pendingEffect) continue;
+    runEffect(dispatch, pendingEffect);
+  }
+}
 
-  return manager;
+export const effectManager = (dispatch) => {
+  const generator = effectGenerator(dispatch);
+  generator.next();
+  return generator;
 };
