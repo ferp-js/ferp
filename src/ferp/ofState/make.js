0d0f03454ccad0b928010b8d229d33861856d4c7
Alex Barry <alex.barry@gmail.com>
2021-03-05 07:51:29 -0500

    Working on testing new ofState subscribe methods

--- src/ferp/ofState/make.js
+++ src/ferp/ofState/make.js
@@ -1,5 +1,9 @@
+export const STOP_GENERATOR = Symbol('StopGenerator');
+
 export const make = (dispatch, generator, ...params) => {
   const iter = generator(dispatch, ...params);
   iter.next();
   return iter;
 };
+
+export const stop = (iterator) => iterator.next(STOP_GENERATOR);
