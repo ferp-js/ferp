9a9ada320cb922e42b5e433035493ebf1c884ea0
Alex Barry <alex.barry@gmail.com>
2022-01-15 12:29:57 -0500

    Ensure all expectations are met
     - Make tester.hit() return all hit effects, regardless of them being expected
     - Allow effects.none() to be annotated
     - Remove resolveAllEffects, I don't think there would be a case when you wouldn't want that realistically
     - Enable developer to ignore effet.none() while testing

--- src/ferp/stages/effectStage.js
+++ src/ferp/stages/effectStage.js
@@ -9,30 +9,32 @@ const asPromise = (value) => {
 const defaultMiddleware = (v) => v;
 
 export const runEffect = (dispatch, effect, middleware = defaultMiddleware) => {
-  const {
-    type,
-    effects,
-    promise,
-    method,
-    action,
-    name,
-  } = middleware(effect);
-
-  switch (type) {
-    case effectTypes.none:
+  switch (effect.type) {
+    case effectTypes.none: {
+      middleware(effect);
       return undefined;
+    }
 
     case effectTypes.batch:
-      return effects.forEach((fx) => runEffect(dispatch, fx, middleware));
+      const { effects } = middleware(effect);
+      return effects
+        .forEach((fx) => runEffect(dispatch, fx, middleware));
 
-    case effectTypes.defer:
-      return asPromise(promise).then((fx) => runEffect(dispatch, fx, middleware));
+    case effectTypes.defer: {
+      const { promise } = middleware(effect);
+      return asPromise(promise)
+        .then((fx) => runEffect(dispatch, fx, middleware));
+    }
 
-    case effectTypes.thunk:
+    case effectTypes.thunk: {
+      const { method } = middleware(effect);
       return runEffect(dispatch, method(), middleware);
+    }
 
-    case effectTypes.act:
-      return dispatch(action, name);
+    case effectTypes.act: {
+      const { action, annotation } = effect;
+      return dispatch(action, annotation);
+    }
 
     default: {
       const error = new TypeError('Unable to run effect');
--- src/testing/example.test.js
+++ src/testing/example.test.js
@@ -4,30 +4,11 @@ import { tester as ferpTester } from './index.js';
 import * as effects from '../ferp/effects/core.js';
 import { sub } from '../ferp/subscriptions/core.js';
 
-test('shallow testing', async (t) => {
-  const deepAction = (state) => [{ ...state, counter: state.counter + 5 }, effects.none()];
-  const action = (state) => [{ ...state, counter: state.counter + 1 }, effects.act(deepAction)];
-
-  const tester = await ferpTester({ counter: 0 })
-    .willAct('action')
-    .willAct('deepAction')
-    .fromAction(action);
-
-  t.falsy(tester.ok());
-  t.deepEqual(tester.failedOn(), [
-    { type: effects.effectTypes.act, annotation: 'deepAction' },
-  ]);
-  t.deepEqual(tester.state(), {
-    counter: 1,
-  });
-});
-
 test('deep testing', async (t) => {
   const deepAction = (state) => [state, effects.none()];
   const action = (state) => [{ state: state.counter + 1 }, effects.act(deepAction)];
 
   const tester = await ferpTester({ counter: 0 })
-    .resolveAllEffects()
     .willThunk('init')
     .willAct('action')
     .willAct('deepAction')
@@ -44,7 +25,6 @@ test('will wait for promises to resolve', async (t) => {
   ));
 
   const tester = await ferpTester()
-    .resolveAllEffects()
     .willDefer()
     .willAct('action')
     .fromEffect(delay(action));
@@ -58,10 +38,10 @@ test('will batch actions', async (t) => {
   const action3 = (state) => [state, effects.none()];
 
   const tester = await ferpTester({ counter: 0 })
-    .resolveAllEffects()
     .willBatch('multi')
     .willAct('action1')
     .willAct('action2')
+    .willAct('action3')
     .fromEffect(effects.batch([
       effects.act(action1),
       effects.act(action2),
@@ -70,9 +50,96 @@ test('will batch actions', async (t) => {
 
   t.truthy(tester.ok());
 
-  t.deepEqual(tester.missed(), [
-    { type: 'Symbol(act)', annotation: 'action3' },
+  t.deepEqual(tester.missed(), []);
+  t.deepEqual(tester.failedOn(), []);
+});
+
+test('ignores none by default', async (t) => {
+  const { ok, failedOn } = await ferpTester()
+    .willNone('foo')
+    .fromEffect(effects.none('foo'));
+
+  t.falsy(ok());
+
+  t.deepEqual(failedOn(), [
+    'Symbol(none):foo',
   ]);
+});
+
+test('hits none with opt-in', async (t) => {
+  const { ok, failedOn } = await ferpTester()
+    .includeEffectNone()
+    .willNone('foo')
+    .fromEffect(effects.none('foo'));
+
+  t.truthy(ok());
+  t.deepEqual(failedOn(), []);
+});
+
+test('detects additional effects that could be asserted on', async (t) => {
+  const { ok, missed } = await ferpTester()
+    .willBatch('batch')
+    .fromEffect(effects.batch([
+      effects.thunk(() => effects.none(), 'thunk'),
+      effects.act((state) => [state, effects.none()], 'act'),
+    ], 'batch'));
+
+  t.truthy(ok());
+  t.deepEqual(missed(), [
+    'Symbol(thunk):thunk',
+    'Symbol(act):act',
+  ]);
+});
+
+test('fromAction will detect action name from annotation, alias, or name', async (t) => {
+  let result = { ok: () => false };
+
+  result = await ferpTester().willAct('foo').fromAction(((s) => [s, effects.none()]), 'foo');
+  t.log({ missed: result.missed(), failedOn: result.failedOn() });
+  t.truthy(result.ok(), 'from annotation');
+
+  const actionWithAlias = (s) => [s, effects.none()];
+  actionWithAlias.alias = 'foo';
+  result = await ferpTester().willAct('foo').fromAction(actionWithAlias);
+  t.truthy(result.ok(), 'from alias');
+
+  result = await ferpTester().willAct('foo').fromAction(function foo(s) {
+    return [s, effects.none()];
+  });
+  t.truthy(result.ok(), 'from name');
+});
+
+test('reports all hits', async (t) => {
+  const { hit } = await ferpTester()
+    .fromEffect(effects.batch([
+      effects.thunk(() => {
+        return effects.batch([
+          effects.act(() => [null, effects.none()], 'act1'),
+        ], 'batch2');
+      }, 'thunk1'),
+      effects.act(() => [null, effects.none()], 'act2'),
+    ], 'batch1'));
+
+  t.deepEqual(hit(), [
+    'Symbol(batch):batch1',
+    'Symbol(thunk):thunk1',
+    'Symbol(batch):batch2',
+    'Symbol(act):act1',
+    'Symbol(act):act2',
+  ]);
+});
+
+test('tracks state', async (t) => {
+  const initialState = { count: 0 };
+  const incrementCount = s => [{ ...s, count: s.count + 1 }, effects.none()];
+
+  const { ok, state } = await ferpTester(initialState)
+    .willAct('incrementCount')
+    .fromAction(incrementCount);
+
+  t.truthy(ok());
+
+  t.deepEqual(state(), { count: 1 });
 });
 
 test('can test a subscription', (t) => {
@@ -100,19 +167,19 @@ test('can test a subscription', (t) => {
   const action = (state) => [state, effects.none()];
 
   const tester = ferpTester()
-    .resolveAllEffects()
     .willAct('onClick')
     .fromSubscription(sub(mySub, element, action));
 
   t.is(typeof tester.cancel, 'function');
 
   t.truthy(element.addEventListener.calledOnceWith('click', sinon.match.any));
-  t.falsy(tester.ok());
+  t.falsy(tester.ok(), 'pending actions');
 
   handler();
 
-  t.truthy(tester.ok());
+  t.truthy(tester.ok(), 'all expected actions happened');
 
   tester.cancel();
+
   t.truthy(element.removeEventListener.calledOnceWith('click', sinon.match.any));
 });
